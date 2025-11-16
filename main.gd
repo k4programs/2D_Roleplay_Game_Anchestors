67a93866b8fe85eb73580ab3070ccfc2914c9760
Roger Fernando Bechtloff <rogerb1589@googlemail.com>
2025-11-16 11:42:45 +0100

    feat: Initialise Godot project

---
+++ main.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Projekt Sonnenwende-Gefährten starting up!")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
