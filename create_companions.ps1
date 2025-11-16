3ea8c48d49fcd382b7b174a924a229a2a8046bcc
Roger Fernando Bechtloff <rogerb1589@googlemail.com>
2025-11-16 12:32:39 +0100

    feat(content): Create placeholder companions and NPCs

---
+++ create_companions.ps1
@@ -0,0 +1,6 @@
+for ($i = 2; $i -le 30; $i++) {
+    $name = "Companion" + $i
+    $file_path = "resources/companions/" + $name + ".tres"
+    $content = "[gd_resource type=\"Companion\" load_steps=2 format=3 uid=\"uid://comp" + $i + "]\n\n[ext_resource type=\"Texture2D\" uid=\"uid://d1x2y3z4w5\" path=\"res://assets/art/ui/icon.svg\" id=\"1_icon\"]\n\n[resource]\nname = \"" + $name + "\"\ntexture = ExtResource(\"1_icon\")\nmax_hp = " + (10 + ($i * 2)) + "\nhp = " + (10 + ($i * 2)) + "\nattack = " + (2 + [Math]::Floor($i / 2)) + "\n"
+    Set-Content -Path $file_path -Value $content
+}
