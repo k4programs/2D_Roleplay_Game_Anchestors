0c38b2cb08559d3ac4ba1d3a3af5ab2ea6808db1
Roger Fernando Bechtloff <rogerb1589@googlemail.com>
2025-11-16 12:06:55 +0100

    fix(core): Fix race condition in DialogManager and clean up project

--- scripts/dialog_manager.gd
+++ scripts/dialog_manager.gd
@@ -5,7 +5,7 @@ var dialog_box_instance
 
 func _ready():
 	dialog_box_instance = dialog_box_scene.instantiate()
-	get_tree().get_root().add_child(dialog_box_instance)
+	get_tree().get_root().call_deferred("add_child", dialog_box_instance)
 
 func show_dialog(character_name, text):
 	dialog_box_instance.show_dialog(character_name, text)
